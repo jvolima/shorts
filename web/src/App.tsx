0c720966d6a143b80f7878e2045c2a2c26cbec58
João Vitor Lima <jvolima2004@gmail.com>
2022-09-26 10:53:28 -0300

    Adicionando thumbnail no frontend

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -10,7 +10,7 @@ interface Video {
 
 export function App() {
   const [videos, setVideos] = useState<Video[]>([]);
-  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [selectedVideoId, setSelectedVideoId] = useState("");
 
   useEffect(() => {
     async function loadData() {
@@ -27,17 +27,32 @@ export function App() {
     const form = document.querySelector("#uploadVideo");
     const formData = new FormData(form as HTMLFormElement);
 
-    const response = await axios.post("http://localhost:3333/uploadVideo", formData, {
+    await axios.post("http://localhost:3333/uploadVideo", formData, {
       headers: {
         "Content-Type": "multipart/form-data"
       }
-    })
+    });
 
-    console.log(response);
+    alert("Upload feito com sucesso! Recarregue a página para ver o novo vídeo!");
   }
 
   async function handleAddThumbnail(event: FormEvent) {
     event.preventDefault();
+
+    if(selectedVideoId === "") {
+      return alert("Escolha um vídeo para a thumbnail!");
+    }
+
+    const form = document.querySelector("#addThumbnail");
+    const formData = new FormData(form as HTMLFormElement);
+
+    await axios.post(`http://localhost:3333/video/${selectedVideoId}/thumbnail`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data"
+      }
+    });
+
+    alert("Thumbnail adicionada com sucesso! Recarregue a página para ver a nova thumb!");
   }
 
   return (
@@ -47,7 +62,25 @@ export function App() {
       <form id="uploadVideo" onSubmit={handleUploadVideo} method="post" encType="multipart/form-data">
         <div className="mt-10 flex flex-col gap-2">
           <label htmlFor="video" className="text-2xl text-gray-800">Adicionar novo vídeo</label>
-          <input type="file" name="video" id="video" onChange={(e) => setSelectedFile(e.target.files![0])} />
+          <input type="file" name="video" id="video" />
+        </div>
+        <button type="submit" className="flex items-center justify-center py-2 px-4 bg-violet-800 text-white mt-6 rounded-md">
+          Enviar 
+        </button>
+      </form>
+
+      <form id="addThumbnail" onSubmit={handleAddThumbnail} method="post" encType="multipart/form-data">
+        <div className="mt-10 flex flex-col gap-2">
+          <label htmlFor="thumbnail" className="text-2xl text-gray-800">Adicionar thumbnail</label>
+          <input type="file" name="thumbnail" id="thumbnail" />
+        </div>
+        <div className="mt-4 flex flex-col gap-4">
+          {videos.filter(video => video.thumbnail_name === null).map(video => (
+            <div key={video.id} className="flex gap-2 items-center">
+              <label htmlFor="selectVideo" className="text-lg text-gray-700">{video.video_name}</label>
+              <input type="radio" name="selectVideo" value={video.id} id="selectVideo" onChange={(e) => setSelectedVideoId(e.target.value)} />
+            </div>
+          ))}
         </div>
         <button type="submit" className="flex items-center justify-center py-2 px-4 bg-violet-800 text-white mt-6 rounded-md">
           Enviar 
